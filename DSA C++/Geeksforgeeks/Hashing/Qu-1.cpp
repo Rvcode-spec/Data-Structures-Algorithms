cc09c33e028c7fa41941c33d61b2f0e1f7f54576
Ravi Shankar Singh <civilstudy2024@gmail.com>
2024-08-22 17:40:10 +0530

    Array Linear

---
+++ DSA C++/Geeksforgeeks/Hashing/Qu-1.cpp
@@ -0,0 +1,18 @@
+// //Given an array, find the difference between highest occurrence and least occurrence of any number in an array.
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+
+//         int arr[]=  {7, 8, 4, 5, 4, 1, 1, 7, 7, 2, 5};
+//         int size = sizeof(arr)/sizeof(arr[0]);
+
+
+// int freq;
+//     for(int i=0;i<size;i++){
+//             freq[arr[i]]++; 
+//         }
+
+        
+// }
